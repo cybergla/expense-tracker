e69866c98a28410a7255e880dbbe60413e4aa3c8
Tanay Deshmukh <tanaydeshmukh@gmail.com>
2018-12-16 21:14:52 +0400

    Added balance

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,12 @@ router.get('/', function(req, res, next) {
 
 router.get('/addExpense', function(req, res, next) {
   doc.useServiceAccountAuth(creds_json, function (err) {
-    if (err) console.log(err);
+    if (err) {
+      console.log(err);
+      res.send('Error');
+      return;
+    }
+
     doc.getInfo(function(err, info) {
       for (let i = 0; i< info.worksheets.length; i++) {
         var sheet = info.worksheets[i];
@@ -55,5 +60,70 @@ router.get('/addExpense', function(req, res, next) {
   });
 });
 
+router.get('/getBalance', function(req, res, next) {
+  doc.useServiceAccountAuth(creds_json, function (err) {
+    if (err) {
+      console.log(err);
+      res.send('Error');
+      return;
+    }
+
+    doc.getInfo(function(err, info) {
+      for (let i = 0; i< info.worksheets.length; i++) {
+        var sheet = info.worksheets[i];
+        
+        var month = new Date().getMonth();
+        month = months[month];
+
+        if (sheet.title === month){
+          // console.log(month);
+          // console.log(sheet.title);
+          break;
+        }
+      }
+
+      var date = new Date().getDate();
+      var week_num = getWeek(date);
+      var rows = week_to_row[week_num];
+
+      sheet.getCells({
+          'min-row':rows[0],
+          'max-row':rows[1],
+          'min-col':12,
+          'max-col':12
+        }, function (err, cells) {
+          res.send({
+            "tanay":cells[0].value,
+            "manasa":cells[1].value
+          });
+      });
+
+
+    });
+  });
+});
+
+
+function getWeek(date) {
+  if (date <= 7){
+    return 1;
+  }
+  else if (date > 7 && date <= 14){
+    return 2;
+  }
+  else if (date > 14 && date <= 21){
+    return 3;
+  }
+  else if (date > 21 && date <= 31){
+    return 4;
+  }
+}
+
+week_to_row = {
+  1: [10,11],
+  2: [15,16],
+  3: [20,21],
+  4: [25,26]
+}
 
 module.exports = router;
